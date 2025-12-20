50911999d662ac1f466efa254f4766a6a82a549b
Rene Lindeboom <rene.turok13@gmail.com>
2025-12-20 21:15:10 +0100

    Maybe fix for request lockup

--- src/structures/SyncedRequestHandler.js
+++ src/structures/SyncedRequestHandler.js
@@ -4,39 +4,49 @@ class SyncedRequestHandler {
     constructor(ipc, options) {
         this.ipc = ipc;
         this.timeout = options.timeout + 1000;
+        this.activeRequests = new Set(); // Track active requests
     }
 
     request(method, url, auth, body, file, _route, short) {
         return new Promise((resolve, reject) => {
             let stackCapture = new Error().stack;
-
             let requestID = crypto.randomBytes(16).toString('hex');
+            
+            // Ensure unique requestID
+            while (this.activeRequests.has(requestID)) {
+                requestID = crypto.randomBytes(16).toString('hex');
+            }
+            this.activeRequests.add(requestID);
 
             if (file && file.file) file.file = Buffer.from(file.file).toString('base64');
 
-            process.send({ name: 'apiRequest', requestID, method, url, auth, body, file, _route, short });
+            const cleanup = () => {
+                clearTimeout(timeout);
+                this.ipc.unregister(`apiResponse.${requestID}`);
+                this.activeRequests.delete(requestID);
+            };
 
             let timeout = setTimeout(() => {
-                reject(new Error(`Request timed out (>${this.timeout}ms) on ${method} ${url}`));
-
-                this.ipc.unregister(`apiResponse.${requestID}`);
+                cleanup();
+                reject(new Error(`Request timed out (>${this.timeout}ms) on ${method} ${url} [ID: ${requestID}]`));
             }, this.timeout);
 
             this.ipc.register(`apiResponse.${requestID}`, data => {
-                if (data.err) {
-                    let error = new Error(data.err.message);
-
-                    error.stack = data.err.stack + '\n' + stackCapture.substring(stackCapture.indexOf('\n') + 1);
-                    error.code = data.err.code;
-
-                    reject(error);
-                } else {
-                    resolve(data.data);
+                try {
+                    if (data.err) {
+                        let error = new Error(data.err.message);
+                        error.stack = data.err.stack + '\n' + stackCapture.substring(stackCapture.indexOf('\n') + 1);
+                        error.code = data.err.code;
+                        reject(error);
+                    } else {
+                        resolve(data.data);
+                    }
+                } finally {
+                    cleanup();
                 }
-
-                clearTimeout(timeout);
-                this.ipc.unregister(`apiResponse.${requestID}`);
             });
+
+            process.send({ name: 'apiRequest', requestID, method, url, auth, body, file, _route, short });
         });
     }
 }
